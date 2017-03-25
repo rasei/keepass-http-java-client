fba2ee60c7d5f7f9e3243ff6f5870f4481c53b02
Ralf Seidengarn <rasei@users.noreply.github.com>
2017-03-25 15:25:51 +0100

    version 1.2: updated dependencies

--- src/main/java/net/seidengarn/keepasshttp/client/KeePassHttpConnector.java
+++ src/main/java/net/seidengarn/keepasshttp/client/KeePassHttpConnector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Ralf Seidengarn
+ * Copyright 2017 Ralf Seidengarn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,20 +23,25 @@ import net.seidengarn.keepasshttp.client.exception.KeePassHttpNotAssociatedExcep
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.RequestEntity;
-import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.math.RandomUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 /**
  * Connector for communication with a local KeePass with installed KeePassHttp-Plugin. By default the key and id will be
@@ -107,9 +112,6 @@ public class KeePassHttpConnector {
             submitUrl = url;
          }
 
-         HttpClient client = new HttpClient();
-         client.getHttpConnectionManager().getParams().setSoTimeout(10000);
-
          String iv = generateIv();
 
          Map<String, Object> map = new HashMap<>();
@@ -119,29 +121,8 @@ public class KeePassHttpConnector {
          map.put("Verifier", Base64.getEncoder().encodeToString(EncryptionUtil.encrypt(iv, iv, key)));
          map.put("Url", Base64.getEncoder().encodeToString(EncryptionUtil.encrypt(url, iv, key)));
          map.put("SubmitUrl", Base64.getEncoder().encodeToString(EncryptionUtil.encrypt(submitUrl, iv, key)));
-         String request = JSONParser.compose(map);
 
-         PostMethod postMethod = new PostMethod("http://localhost:" + port);
-         RequestEntity requestEntity = new StringRequestEntity(request, "text/xml", "UTF-8");
-         postMethod.setRequestEntity(requestEntity);
-         int result = client.executeMethod(postMethod);
-         if (result != 200) {
-            throw new KeePassHttpCommunicationException("Communication with KeePass failed, http-returncode is "
-                                                        + result + ", expected 200");
-         }
-
-         String responseBody = postMethod.getResponseBodyAsString();
-         map = (Map<String, Object>) JSONParser.parse(responseBody);
-
-         if (map == null || map.get("Success") == null) {
-            throw new KeePassHttpCommunicationException(
-                     "Communication with KeePass failed, response from KeePassHttp is invalid");
-         }
-
-         if (!map.get("Success").equals("true")) {
-            throw new KeePassHttpCommunicationException(
-                     "Communication with KeePass failed, call of get-logins with no success (access may be declined by user)");
-         }
+         map = communicate(map);
 
          iv = (String) map.get("Nonce");
          List<KeePassLogin> loginList = new ArrayList<>();
@@ -164,8 +145,6 @@ public class KeePassHttpConnector {
          return loginList;
       } catch (EncryptionException e) {
          throw new KeePassHttpCommunicationException("Communication with KeePass failed", e);
-      } catch (IOException e) {
-         throw new KeePassHttpCommunicationException("Communication with KeePass failed", e);
       }
    }
 
@@ -176,7 +155,7 @@ public class KeePassHttpConnector {
    private void loadKey() {
       if (keyFile != null) {
          try {
-            String data = FileUtils.readFileToString(keyFile);
+            String data = FileUtils.readFileToString(keyFile, Charset.forName("UTF-8"));
             Map<String, Object> map = (Map<String, Object>) JSONParser.parse(data);
             id = (String) map.get("Id");
             key = (String) map.get("Key");
@@ -199,14 +178,13 @@ public class KeePassHttpConnector {
             map.put("Id", id);
             String data = JSONParser.compose(map);
 
-            FileUtils.writeStringToFile(keyFile, data);
+            FileUtils.writeStringToFile(keyFile, data, Charset.forName("UTF-8"));
          } catch (IOException e) {
             throw new KeePassHttpException("Exception while storing the key to communicate with KeePass", e);
          }
       }
    }
 
-   @SuppressWarnings("unchecked")
    void testAssociate() throws KeePassHttpException {
       if (id == null || key == null) {
          associate();
@@ -221,18 +199,39 @@ public class KeePassHttpConnector {
          map.put("Id", id);
          map.put("Nonce", iv);
          map.put("Verifier", verifier);
-         HttpClient client = new HttpClient();
-         client.getHttpConnectionManager().getParams().setSoTimeout(10000);
-         PostMethod postMethod = new PostMethod("http://localhost:" + port);
-         RequestEntity requestEntity = new StringRequestEntity(JSONParser.compose(map), "text/xml", "UTF-8");
-         postMethod.setRequestEntity(requestEntity);
-         int result = client.executeMethod(postMethod);
-         if (result != 200) {
+         map = communicate(map);
+      } catch (EncryptionException e) {
+         throw new KeePassHttpCommunicationException("Communication with KeePass failed", e);
+      }
+   }
+
+   /**
+    * Doing the communication with KeePass
+    *
+    * @param map request map
+    * @return response map
+    */
+   @SuppressWarnings("unchecked")
+   private Map<String, Object> communicate(Map<String, Object> map) throws KeePassHttpCommunicationException,
+            KeePassHttpNotAssociatedException {
+      try {
+
+         RequestConfig config = RequestConfig.custom().setConnectTimeout(10000).setConnectionRequestTimeout(10000)
+                  .setSocketTimeout(10000).build();
+         CloseableHttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+
+         String request = JSONParser.compose(map);
+         HttpPost postMethod = new HttpPost("http://localhost:" + port);
+         StringEntity requestEntity = new StringEntity(request, "UTF-8");
+         postMethod.setEntity(requestEntity);
+         HttpResponse httpResponse = client.execute(postMethod);
+         int result = httpResponse.getStatusLine().getStatusCode();
+         if (result != HttpStatus.SC_OK) {
             throw new KeePassHttpCommunicationException("Communication with KeePass failed, http-returncode is "
                                                         + result + ", expected 200");
          }
 
-         String responseBody = postMethod.getResponseBodyAsString();
+         String responseBody = IOUtils.toString(httpResponse.getEntity().getContent(), Charset.forName("UTF-8"));
          map = (Map<String, Object>) JSONParser.parse(responseBody);
 
          if (map == null || map.get("Success") == null) {
@@ -244,11 +243,10 @@ public class KeePassHttpConnector {
             throw new KeePassHttpNotAssociatedException(
                      "Communication with KeePass failed, client is not associated with KeePassHttp");
          }
-      } catch (EncryptionException e) {
-         throw new KeePassHttpCommunicationException("Communication with KeePass failed", e);
       } catch (IOException e) {
          throw new KeePassHttpCommunicationException("Communication with KeePass failed", e);
       }
+      return map;
    }
 
    private String generateIv() {
@@ -259,7 +257,6 @@ public class KeePassHttpConnector {
       return Base64.getEncoder().encodeToString(ivArr);
    }
 
-   @SuppressWarnings("unchecked")
    void associate() throws KeePassHttpException {
       if (key == null) {
          key = generateIv();
@@ -274,35 +271,12 @@ public class KeePassHttpConnector {
          map.put("Key", key);
          map.put("Nonce", iv);
          map.put("Verifier", verifier);
-         HttpClient client = new HttpClient();
-         client.getHttpConnectionManager().getParams().setSoTimeout(10000);
-         PostMethod postMethod = new PostMethod("http://localhost:" + port);
-         RequestEntity requestEntity = new StringRequestEntity(JSONParser.compose(map), "text/xml", "UTF-8");
-         postMethod.setRequestEntity(requestEntity);
-         int result = client.executeMethod(postMethod);
-         if (result != 200) {
-            throw new KeePassHttpCommunicationException("Communication with KeePass failed, http-returncode is "
-                                                        + result + ", expected 200");
-         }
 
-         String responseBody = postMethod.getResponseBodyAsString();
-         map = (Map<String, Object>) JSONParser.parse(responseBody);
-
-         if (map == null || map.get("Success") == null) {
-            throw new KeePassHttpCommunicationException(
-                     "Communication with KeePass failed, response from KeePassHttp is invalid");
-         }
-
-         if (!map.get("Success").equals("true")) {
-            throw new KeePassHttpNotAssociatedException(
-                     "Communication with KeePass failed, client could not associate with KeePassHttp, maybe declined by user");
-         }
+         map = communicate(map);
 
          id = (String) map.get("Id");
          storeKey();
       } catch (EncryptionException e) {
-         throw new KeePassHttpCommunicationException("Communication with KeePass failed", e);
-      } catch (IOException e) {
          throw new KeePassHttpCommunicationException("Communication with KeePass failed", e);
       }
    }
